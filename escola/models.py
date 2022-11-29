3d123521159e71610679ee64e1ee67ce7700ef52
eliascomh <grilocamusiano@gmail.com>
2022-11-29 15:48:40 -0300

    Inserindo matrículas de um curso e autenticação basica

--- escola/models.py
+++ escola/models.py
@@ -10,11 +10,11 @@ class Aluno(models.Model):
         return self.nome
     
 class Curso(models.Model):
-    NIVEL = {
+    NIVEL = (
         ('B', 'Básico'),
         ('I', 'Itermediário'),
         ('A', 'Avançado')
-    }
+    )
 
     codigo_curso = models.CharField(max_length=10)
     descricao = models.CharField(max_length=100)
@@ -22,3 +22,16 @@ class Curso(models.Model):
 
     def __str__(self):
         return self.descricao
+    
+
+class Matricula(models.Model):
+    PERIODO = (
+        ('M', 'Matutino'),
+        ('V', 'Vespertino'),
+        ('N', 'Noturno')
+    )
+
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    periodo = models.CharField(max_length=1, choices=PERIODO, blank=False, null=False, default='M')
+
